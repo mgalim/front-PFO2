c6bd9855f2ddc5a394f75869d92515bdce85588d
Marcelo Galimberti <galimbertimarcelo@gmail.com>
2025-04-16 10:45:52 -0300

    add dinamic movies content

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Popup informativo
+
 const iconos = {
   error: "./img/danger.svg",
   success: "./img/success.svg",
@@ -5,11 +7,8 @@ const iconos = {
   info: "./img/info.svg",
 };
 
-const btnTema = document.getElementById("btn-theme");
-const formulario = document.getElementById("formulario");
-const popup = document.getElementById("popup");
 const mensaje = document.getElementById("mensaje");
-const inputs = formulario.querySelectorAll("input");
+const popup = document.getElementById("popup");
 const icono = popup.querySelector("img");
 
 function showPopup(msg, type) {
@@ -22,6 +21,10 @@ function showPopup(msg, type) {
   }, 2000);
 }
 
+// Cambio de tema
+
+const btnTema = document.getElementById("btn-theme");
+
 btnTema.addEventListener("click", () => {
   btnTema.classList.toggle("active");
   document.body.classList.toggle("dark-theme");
@@ -30,6 +33,11 @@ btnTema.addEventListener("click", () => {
     btnTema.classList.remove("active");
   }, 1000);
 });
+
+// Validaciones formulario contacto
+
+const formulario = document.getElementById("formulario");
+const inputs = formulario.querySelectorAll("input");
 
 inputs.item(3).addEventListener("input", (event) => {
   event.stopPropagation();
@@ -63,6 +71,66 @@ formulario.addEventListener("submit", function (event) {
   showPopup("Los datos se enviaron con exito!!!", "success");
   formulario.reset();
 });
+
+// Renderizado de peliculas
+
+const peliculas = [
+  {
+    titulo: "Pulp Fiction",
+    imagen: "img/pulp.webp",
+    alt: "Película 1",
+    descripcion:
+      "Una obra maestra del cine dirigida por Quentin Tarantino, con una narrativa no lineal única. Llena de diálogos y personajes únicos.",
+    enlace: "https://en.wikipedia.org/wiki/Pulp_Fiction",
+  },
+  {
+    titulo: "Interstellar",
+    imagen: "img/inter.webp",
+    alt: "Película 2",
+    descripcion:
+      "Una película de ciencia ficción con una gran profundidad emocional. Explora los viajes espaciales y el tiempo con una narrativa impactante.",
+    enlace: "https://en.wikipedia.org/wiki/Interstellar_(film)",
+  },
+  {
+    titulo: "El Club de la Pelea",
+    imagen: "img/club.webp",
+    alt: "Película 3",
+    descripcion:
+      "Un thriller psicológico con una trama fascinante. Basada en la novela de Chuck Palahniuk, es una historia llena de giros inesperados.",
+    enlace: "https://es.wikipedia.org/wiki/Fight_Club",
+  },
+];
+
+const seccionPeliculas = document.querySelector("#peliculas");
+
+peliculas.forEach((peli) => {
+  const enlace = document.createElement("a");
+  enlace.href = peli.enlace;
+  enlace.target = "_blank";
+  enlace.rel = "noopener noreferrer";
+
+  const article = document.createElement("article");
+  article.classList.add("pelicula");
+
+  const img = document.createElement("img");
+  img.src = peli.imagen;
+  img.alt = peli.alt;
+  img.width = 200;
+
+  const h3 = document.createElement("h3");
+  h3.textContent = peli.titulo;
+
+  const p = document.createElement("p");
+  p.textContent = peli.descripcion;
+
+  article.appendChild(img);
+  article.appendChild(h3);
+  article.appendChild(p);
+  enlace.appendChild(article);
+  seccionPeliculas.appendChild(enlace);
+});
+
+// Cuenta regresiva para entregar trabajo
 
 function cuentaRegresiva() {
   const fechaLimite = new Date("2025-05-05T23:59:59").getTime();
